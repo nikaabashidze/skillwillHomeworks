ee95d30a31d1882e0cec8af6addd1ceba0652e00
Nika Abashidze <nicholas.abashidze@gmail.com>
2024-01-23 01:43:53 +0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,15 @@ import Homework11 from './layout/skillwillHomework11/skillwill'
 import Homework12 from './layout/skillwillHomework12/homework12'
 
 function App() {
-  
-
-  
   return (
     <Routes>
-    <Route path="/skillwillHomeworks" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/services" element={<Services />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="/homework" element={<Tours />} />
-    <Route path="/skillwillHomework11" element={<Homework11 />} />
-    <Route path="/skillwillHomework12" element={<Homework12 />} />
+      <Route path="/skillwillHomeworks" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/homework" element={<Tours />} />
+      <Route path="/skillwillHomework11" element={<Homework11 />} />
+      <Route path="/skillwillHomework12" element={<Homework12 />} />
     </Routes>
   );
 }
